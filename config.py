731a5a963a8f4018a519cf97ae1adc755ca6a9aa
TUHIN ROY <tuhin.roy2025@lpu.in>
2026-03-21 01:35:26 +0530

    Add TWELVEDATA_API_KEY to Config class

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    TELEGRAM_BOT_TOKEN: str  = os.getenv("TELEGRAM_BOT_TOKEN", "")
-    FINNHUB_API_KEY: str     = os.getenv("FINNHUB_API_KEY", "")
+    TELEGRAM_BOT_TOKEN: str = os.getenv("TELEGRAM_BOT_TOKEN", "")
+    TWELVEDATA_API_KEY: str = os.getenv("TWELVEDATA_API_KEY", "")
 
     def __init__(self):
         if not self.TELEGRAM_BOT_TOKEN:
             raise ValueError("TELEGRAM_BOT_TOKEN is not set")
-        if not self.FINNHUB_API_KEY:
-            raise ValueError("FINNHUB_API_KEY is not set")
+        if not self.TWELVEDATA_API_KEY:
+            raise ValueError("TWELVEDATA_API_KEY is not set")
